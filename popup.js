5be531293b8880e9925481dd1d07e74ae7360ed5
2jh1230 <2jh1230@naver.com>
2025-04-01 14:51:09 +0900

    google drive api 연동

--- popup.js
+++ popup.js
@@ -1,15 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const toggle = document.getElementById('tracking-toggle');
-  
-    // 초기 상태 로드
-    chrome.storage.sync.get(['trackingEnabled'], (result) => {
-      const isEnabled = result.trackingEnabled ?? true;
-      toggle.checked = isEnabled;
-    });
-  
-    // 상태 변경 시 저장
-    toggle.addEventListener('change', () => {
-      const isChecked = toggle.checked;
-      chrome.storage.sync.set({ trackingEnabled: isChecked });
-    });
+  const toggle = document.getElementById('tracking-toggle');
+
+  // 초기 상태 로드
+  chrome.storage.sync.get(['trackingEnabled'], (result) => {
+    const isEnabled = result.trackingEnabled ?? true;
+    toggle.checked = isEnabled;
   });
+
+  // 상태 변경 시 저장
+  toggle.addEventListener('change', () => {
+    const isChecked = toggle.checked;
+    chrome.storage.sync.set({ trackingEnabled: isChecked });
+  });
+});
+
+document.getElementById('upload-button').addEventListener('click', () => {
+  chrome.storage.sync.get(['visitedUrls'], (data) => {
+    const urls = data.visitedUrls || [];
+    chrome.runtime.sendMessage({ action: 'uploadToDrive', data: urls });
+  });
+});
+
